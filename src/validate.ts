6d333fb1a91cd18e853878bae84db1a0c7fbcbe4
Kamil Matysiewicz <kamil@aexol.com>
2018-02-12 11:22:35 +0100

    Logging

--- src/validate.ts
+++ src/validate.ts
@@ -4,7 +4,10 @@ export class ValidatePlugin {
   constructor(private handler: IHandler, public rules?: object) {}
   public handle(ctx: ISyncanoContext, syncano: object): Promise<IResponse|IResponsePayload|IResponseStatus> {
 
-    return validate(ctx.args, this.rules).then(() => this.handler(ctx, syncano));
+    return validate(ctx.args, this.rules).then(() => this.handler(ctx, syncano)).catch((e: Error) => {
+      console.log(JSON.stringify(e));
+      throw e;
+    });
   }
 }
 
