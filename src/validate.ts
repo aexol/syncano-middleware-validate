93feec3094fc906df418ce1a2b3042d75158860e
Kamil Matysiewicz <kamil@aexol.com>
2018-02-12 12:11:37 +0100

    Better error handling.

--- src/validate.ts
+++ src/validate.ts
@@ -4,16 +4,15 @@ export class ValidatePlugin {
   constructor(private handler: IHandler, public rules?: object) {}
   public handle(ctx: ISyncanoContext, syncano: object): Promise<IResponse|IResponsePayload|IResponseStatus> {
 
-    return validate(ctx.args, this.rules).then(() => this.handler(ctx, syncano)).catch((e: Error) => {
-      console.log(JSON.stringify(e));
-      throw e;
-    });
+    return validate(ctx.args, this.rules).catch((e: Error): void => {
+      throw {
+        payload: e,
+        status: 400,
+      };
+    }).then(() => this.handler(ctx, syncano));
   }
 }
 
-export default (handler: IHandler, rules?: object): IHandler => {
-  return (ctx: ISyncanoContext, syncano: object): Promise<IResponse|IResponsePayload|IResponseStatus> => {
-    const validator = new ValidatePlugin(handler, rules);
-    return validator.handle(ctx, syncano);
-  };
-};
+export default (handler: IHandler, rules?: object): IHandler =>
+  (ctx: ISyncanoContext, syncano: object): Promise<IResponse|IResponsePayload|IResponseStatus> =>
+    (new ValidatePlugin(handler, rules)).handle(ctx, syncano);
