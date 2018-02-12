2d736096434ff62b1829e9cea44589fb30addde2
Kamil Matysiewicz <kamil@aexol.com>
2018-02-12 11:08:25 +0100

    Fix dot

--- lib/validate.d.ts
+++ lib/validate.d.ts
@@ -1,1 +1,9 @@
-export {};
+import { IHandler, IResponse, IResponsePayload, IResponseStatus, ISyncanoContext } from 'syncano-middleware';
+export declare class ValidatePlugin {
+    private handler;
+    rules: object | undefined;
+    constructor(handler: IHandler, rules?: object | undefined);
+    handle(ctx: ISyncanoContext, syncano: object): Promise<IResponse | IResponsePayload | IResponseStatus>;
+}
+declare const _default: (handler: IHandler, rules?: object | undefined) => IHandler;
+export default _default;
